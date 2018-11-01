33db716e5ca86e43f96f0a85ada9437d1b10088c
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2018-10-31 20:04:33 -0700

    expanding the project. Thinking of writting more chapters

---
+++ assets/javascript/script.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+  el: "#container",
+  data: {
+    hello: "Hello there. This is the vue related script",
+    chapters: {
+      "000": "Reset",
+      "001": "Specificity",
+      "002": "Box Model",
+      "003": "Positioning",
+      "004": "Typography", //Do I need to change the positioning of this so that it is after the backgrounds?
+      "005": "Backgrounds",
+      "006": "Responsive Design",
+      "007": "more a",
+      "008": "More b",
+      "009": "More c",
+      "010": "This is ten"
+    }
+  }
+});
