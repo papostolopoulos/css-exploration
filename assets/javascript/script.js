19789fcebe72d8a6123e4276ba652021a1027e13
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2018-11-07 22:24:05 -0800

    worked on the anchor links

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,19 +2,7 @@ var app = new Vue({
   el: "#container",
   data: {
     hello: "Hello there. This is the vue related script",
-    chapters: {
-      "000": "Reset",
-      "001": "Specificity",
-      "002": "Box Model",
-      "003": "Positioning",
-      "004": "Typography", //Do I need to change the positioning of this so that it is after the backgrounds?
-      "005": "Backgrounds",
-      "006": "Transforms",
-      "007": "more a",
-      "008": "More b",
-      "009": "More c",
-      "010": "This is ten"
-    },
+    chapters: [],
     activePage: "Index",
     indexPage: "",
     activeSection: "",
@@ -24,30 +12,43 @@ var app = new Vue({
   }, //End of data
   methods: {
     activeTag(event){
-      console.log(event);
       let list = event.path[3].children;
       for (var i = 0; i < list.length; i++) {
-        console.log(list[i]);
         var anchorClass = list[i].children[0].children[0].className;
         list[i].children[0].children[0].className = anchorClass.replace(/\s?anchorActiveTag/, "");
       }
 
-      event.target.className += "anchorActiveTag";
+      if (!event.target.className.includes("anchorActiveTag")) event.target.className += "anchorActiveTag";
     },
-    whatIsActive(){
-      console.log(document.querySelectorAll("h1,h2,h3,h4,h5"));
-      let hdngsList = document.querySelectorAll("h1,h2,h3,h4,h5,h6");
-      hdngsList.forEach(el=>{
-        console.log(el.localName);
-        console.log(el.innerText);
+    headingsFormation(){
+      let hList = document.querySelectorAll("h1,h2,h3,h4,h5,h6");
+
+      hList.forEach((el, idx)=>{
+        // console.log(el.innerText);
         let newHeading = {};
-        newHeading.heading = el.localName;
-        newHeading.text = el.innerText;
-        this.makeAMess.push(newHeading);
+
+        //If statement so that the headings in the <aside> tag are disregarded
+        if (!el.innerText.includes("chapter.text")) {
+          newHeading.heading = el.localName;
+          newHeading.text = el.innerText;
+          this.chapters.push(newHeading);
+        }
       });
+    },
+    makeId(){
+      let hList = document.querySelectorAll("h1,h2,h3,h4,h5,h6");
+
+      for(let i = 0; i < hList.length; i++){
+        hList[i].id = hList[i].localName + "_" + hList[i].innerText.replace(/\s/g, "-");
+      }
     }
   }, //End of methods
+  computed: {
+  },
   beforeMount(){
-    this.whatIsActive();
+    this.headingsFormation();
+  },
+  mounted(){
+    this.makeId();
   }
 });
